d87dad931d34965ecc9298ca35f8f441fe76342a
Jonas Andersson <jonas8andersson@gmail.com>
2018-03-14 13:21:27 +0100

    elpa added

---
+++ elpa/challenger-deep-theme-20180223.644/challenger-deep-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "challenger-deep-theme" "20180223.644" "challenger-deep Theme" '((emacs "24")) :commit "8194c3ade7ba73f6ddfea7dec55f77b5b6fdbcab" :url "https://github.com/challenger-deep-theme/emacs")
