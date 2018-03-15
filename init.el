c63240d7cb2e1e24213d710f61c72a65c7d218c7
Jonas Andersson <jonas8andersson@gmail.com>
2018-03-15 11:11:28 +0100

    added notes.txt and chages to config

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 ; theme color
-(load-theme 'challenger-deep t)
+(load-theme 'wheatgrass t)
 ; start auto-complte with emacs
 (require 'auto-complete)
 (require 'auto-complete-config)
@@ -49,9 +49,69 @@
 		      :include-path '("/inc"
 				      "/inc2"))
 (global-semantic-idle-scheduler-mode 1)
-
+; NYAN cat mode
 (require 'nyan-mode)
-(setq nyan-wavy-trail t)
+;(setq nyan-wavy-trail t)
 (setq nyan-bar-length 20)
 (setq nyan-animate-nyancat t)
 (nyan-mode)
+; Enable Ido mode
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-use-filename-at-point 'guess)
+(ido-mode 1)
+; Use Ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ibuffer-saved-filter-groups
+   '(("c-files-filter"
+      ("C-files-filter"
+       (used-mode . c-mode)))))
+ '(ibuffer-saved-filters
+   '(("programming"
+      (or
+       (derived-mode . prog-mode)
+       (mode . ess-mode)
+       (mode . compilation-mode)))
+     ("text document"
+      (and
+       (derived-mode . text-mode)
+       (not
+	(starred-name))))
+     ("TeX"
+      (or
+       (derived-mode . tex-mode)
+       (mode . latex-mode)
+       (mode . context-mode)
+       (mode . ams-tex-mode)
+       (mode . bibtex-mode)))
+     ("web"
+      (or
+       (derived-mode . sgml-mode)
+       (derived-mode . css-mode)
+       (mode . javascript-mode)
+       (mode . js2-mode)
+       (mode . scss-mode)
+       (derived-mode . haml-mode)
+       (mode . sass-mode)))
+     ("gnus"
+      (or
+       (mode . message-mode)
+       (mode . mail-mode)
+       (mode . gnus-group-mode)
+       (mode . gnus-summary-mode)
+       (mode . gnus-article-mode))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
